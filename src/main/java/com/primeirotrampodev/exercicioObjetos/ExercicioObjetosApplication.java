bc1cb6c05f917fedf7a67a9e18d69f18b4d08f4c
Pablo Henrique <pablohen7ique@gmail.com>
2023-03-30 20:57:50 -0300

    Inicio dos Exercicios

---
+++ src/main/java/com/primeirotrampodev/exercicioObjetos/ExercicioObjetosApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.exercicioObjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioObjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioObjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primeirotrampodev/exercicioObjetos/ExercicioObjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.exercicioObjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioObjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
